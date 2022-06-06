ff0a42bb716a62a35413d6c9cb2ed3f2d7a14f45
vitoria <vitoria@test.com>
2022-06-06 11:16:27 -0300

    Classes abstratas-exercicio

---
+++ pw2-CalculoSalarial/src/Funcionario.java
@@ -0,0 +1,62 @@
+
+public abstract class Funcionario {
+
+	private String nome;
+	private String cpf;
+	private double salarioBase;
+	private double salarioFinal;
+	private double valorBonificacao;
+	
+	public void calcularBonificacao() {
+		valorBonificacao = salarioFinal * 0.05; 
+	}
+	
+	public void cadastrarFuncionario(String nome, String cpf, double salarioBase) {
+		salarioFinal = 0;
+		valorBonificacao = 0;
+	}
+	
+	public abstract void listarFuncionario();
+	public abstract void calcularSalarioFinal(double salarioFinal);
+	public abstract String apresentarSalario();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalarioBase() {
+		return salarioBase;
+	}
+
+	public void setSalarioBase(double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public double getSalarioFinal() {
+		return salarioFinal;
+	}
+
+	public void setSalarioFinal(double salarioFinal) {
+		this.salarioFinal = salarioFinal;
+	}
+
+	public double getValorBonificacao() {
+		return valorBonificacao;
+	}
+
+	public void setValorBonificacao(double valorBonificacao) {
+		this.valorBonificacao = valorBonificacao;
+	}
+}
---
+++ pw2-CalculoSalarial/src/Gerente.java
@@ -0,0 +1,16 @@
+
+public class Gerente extends Funcionario{
+
+	public void listarFuncionario(){
+		
+	}
+	
+	public void calcularSalarioFinal(double salarioFinal) {
+		
+	}
+	
+	public String apresentarSalario() {
+		
+	}
+	
+}
